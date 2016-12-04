3fbd3274fa9daaf2451e6533427c2ebddebb2583
Jacob Bang <julemand101@gmail.com>
2016-12-04 12:41:02 +0100

    Day01: Moved solution for A and B to same file

--- bin/day01/main.dart
+++ bin/day01/main.dart
@@ -1,6 +1,8 @@
 // --- Day 1: No Time for a Taxicab ---
 // http://adventofcode.com/2016/day/1
 
+import 'dart:math';
+
 const String input =
     "R1, L3, R5, R5, R5, L4, R5, R1, R2, L1, L1, R5, R1, L3, L5, L2, R4, L1, "
     "R4, R5, L3, R5, L1, R3, L5, R1, L2, R1, L5, L1, R1, R4, R1, L1, L3, R3, "
@@ -13,10 +15,14 @@ const String input =
     "R5, R1, L1, L4, L5, R2, R4, L3, L5, R2, R3, L4, L1, R2, R3, R5, L2, L3, "
     "R3, R1, R3";
 
+
 enum Direction { NORTH, EAST, SOUTH, WEST }
 
 main(List<String> args) {
   Direction direction = Direction.NORTH;
+  Set<Point> history = new Set<Point>();
+  Point firstPointToVisitTwice = null;
+
   int x = 0;
   int y = 0;
 
@@ -26,22 +32,66 @@ main(List<String> args) {
 
     switch (direction) {
       case Direction.NORTH:
-        y += length;
+        for (int i = 0; i < length; i++) {
+          y += 1;
+
+          if (firstPointToVisitTwice == null) {
+            Point p = new Point(x, y);
+
+            if (history.add(p) == false) {
+              firstPointToVisitTwice = p;
+            }
+          }
+        }
         break;
       case Direction.EAST:
-        x += length;
+        for (int i = 0; i < length; i++) {
+          x += 1;
+
+          if (firstPointToVisitTwice == null) {
+            Point p = new Point(x, y);
+
+            if (history.add(p) == false) {
+              firstPointToVisitTwice = p;
+            }
+          }
+        }
         break;
       case Direction.SOUTH:
-        y -= length;
+        for (int i = 0; i < length; i++) {
+          y -= 1;
+
+          if (firstPointToVisitTwice == null) {
+            Point p = new Point(x, y);
+
+            if (history.add(p) == false) {
+              firstPointToVisitTwice = p;
+            }
+          }
+        }
         break;
       case Direction.WEST:
-        x -= length;
+        for (int i = 0; i < length; i++) {
+          x -= 1;
+
+          if (firstPointToVisitTwice == null) {
+            Point p = new Point(x, y);
+
+            if (history.add(p) == false) {
+              firstPointToVisitTwice = p;
+            }
+          }
+        }
         break;
     }
   }
 
   print("x: ${x}, y: ${y}");
   print("Distance from start (0,0): ${x.abs() + y.abs()}");
+  print("");
+  print("First point to visit twice: $firstPointToVisitTwice");
+  print("Distance from start (0,0): ${firstPointToVisitTwice.x.abs() +
+    firstPointToVisitTwice.y.abs()}");
 }
 
 Direction move(Direction d, String move) {
