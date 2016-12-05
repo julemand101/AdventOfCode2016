eae08bf1b985d93fbc79e9965c7994c23377a8a6
Jacob Bang <julemand101@gmail.com>
2016-12-05 21:15:27 +0100

    Day05b: Solved

--- bin/day05/main.dart
+++ bin/day05/main.dart
@@ -7,6 +7,11 @@ import 'package:crypto/crypto.dart' as crypto;
 const String input = "wtnhxymk";
 
 main(List<String> args) {
+  print("Password A: ${getPasswordA(input)}");
+  print("Password B: ${getPasswordB(input)}");
+}
+
+String getPasswordA(String input) {
   StringBuffer sb = new StringBuffer();
 
   for (int count = 0; sb.length < 8; count++) {
@@ -15,9 +20,30 @@ main(List<String> args) {
 
     if (sum.startsWith("00000")) {
       sb.write(sum[5]);
-      print("$sum => $count");
     }
   }
 
-  print("Password: $sb");
+  return sb.toString();
+}
+
+String getPasswordB(String input) {
+  List<String> list = new List.filled(8, "_");
+
+  for (int count = 0; list.contains("_"); count++) {
+    List<int> encoded = ASCII.encode("$input$count");
+    String sum = crypto.md5.convert(encoded).toString();
+
+    if (sum.startsWith("00000")) {
+      int pos = int.parse(sum[5], onError: (_) => -1);
+
+      if (pos != -1 && pos < list.length && list[pos] == "_") {
+        list[pos] = sum[6];
+        print(list);
+      }
+    }
+  }
+
+  StringBuffer sb = new StringBuffer();
+  list.forEach((String s) => sb.write(s));
+  return sb.toString();
 }
