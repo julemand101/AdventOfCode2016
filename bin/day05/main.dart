2fdf157c6244165bf26d5f2252d77051beee5337
Jacob Bang <julemand101@gmail.com>
2016-12-05 19:50:14 +0100

    Day05: Need crypto API for MD5 hash

--- bin/day05/main.dart
+++ bin/day05/main.dart
@@ -1,6 +1,10 @@
-//
-//
+// --- Day 5: How About a Nice Game of Chess? ---
+// http://adventofcode.com/2016/day/5
+
+import 'package:crypto/crypto.dart' as crypto;
+
+const String input = "wtnhxymk";
 
 main(List<String> args) {
-
+  
 }
