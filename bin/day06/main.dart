538e7688fe98e29bb32ad7a6830ce0969820a9f4
Jacob Bang <julemand101@gmail.com>
2016-12-06 19:24:03 +0100

    Day06a: Solved

--- bin/day06/main.dart
+++ bin/day06/main.dart
@@ -1,6 +1,33 @@
 // --- Day 6: Signals and Noise ---
 // http://adventofcode.com/2016/day/6
 
-main(List<String> args) {
+import 'dart:io';
+import 'dart:convert';
+import 'package:more/collection.dart';
 
+main(List<String> args) async {
+  File inputFile = new File("bin/day06/input.txt");
+  List<Multiset> msg =
+      new List.generate(8, (_) => new Multiset(), growable: false);
+
+  await inputFile
+      .openRead()
+      .transform(new AsciiDecoder())
+      .transform(new LineSplitter())
+      .forEach((String line) {
+    for (int i = 0; i < line.length; i++) {
+      String letter = line[i];
+      Multiset multiset = msg[i];
+
+      multiset.add(letter);
+    }
+  });
+
+  StringBuffer sb = new StringBuffer();
+  for (Multiset s in msg) {
+    sb.write(s.distinct
+        .fold("", (prev, element) => (s[element] > s[prev]) ? element : prev));
+  }
+
+  print("Message: $sb");
 }
