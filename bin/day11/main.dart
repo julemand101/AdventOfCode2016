48a0ab0e87e8be819b2fbaf16df94a0a080b57d8
Jacob Bang <julemand101@gmail.com>
2016-12-11 15:06:56 +0100

    Day11: Prepare for a nightmare...

---
+++ bin/day11/main.dart
@@ -0,0 +1,28 @@
+// --- Day 11: Radioisotope Thermoelectric Generators ---
+// http://adventofcode.com/2016/day/11
+
+/*
+ * The first floor contains:
+ *  * a polonium generator
+ *  * a thulium generator
+ *  * a thulium-compatible microchip
+ *  * a promethium generator
+ *  * a ruthenium generator
+ *  * a ruthenium-compatible microchip
+ *  * a cobalt generator
+ *  * a cobalt-compatible microchip.
+ *
+ * The second floor contains:
+ *  * a polonium-compatible microchip
+ *  * a promethium-compatible microchip.
+ *
+ * The third floor contains:
+ *  * nothing relevant.
+ *
+ * The fourth floor contains:
+ *  * nothing relevant.
+ */
+
+main(List<String> args) {
+
+}
